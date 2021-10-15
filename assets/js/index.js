eae866ad809889c15a57367a6f9549d6e3ab3f3e
BahamJustin <bahamjustin@outlook.com>
2021-10-15 02:00:38 -0500

    Start Game function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-console.log("WTF");
+var time = 30;
+
+// Starts Game
+$("#start-btn").click(function startGame() {
+  // Test start
+  console.log("Start Game");
+
+  // hide start menu
+  $("#start-page").hide();
+
+  // Starts 30 sec timer
+  setInterval(startTimer, 1000);
+
+  function startTimer() {
+    time--;
+    console.log(time);
+    if (time === 0) {
+      return;
+    }
+  }
+
+  //shows first question
+  $("#questions").removeClass("d-none");
+});
