09e5a408d11937550db7b681d50951bf60a36d3c
BahamJustin <bahamjustin@outlook.com>
2021-10-17 22:57:34 -0500

    game over/ high score

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 var time = 30;
 
+var highScore = 0;
+
 // Starts Game
 $("#start-btn").click(function startGame() {
   // Test start
@@ -8,17 +10,42 @@ $("#start-btn").click(function startGame() {
   // hide start menu
   $("#start-page").hide();
 
-  // Starts 30 sec timer
-  setInterval(startTimer, 1000);
+  // Timer Function
+  var timeInterval = setInterval(startTimer, 1000);
 
   function startTimer() {
-    time--;
     console.log(time);
-    if (time === 0) {
-      return;
+    if (time > 1) {
+      $("#timer").text(time + " seconds remaining");
+      time--;
+    } else if (time === 1) {
+      $("#timer").text(time + " second remaining");
+      time--;
+    } else {
+      $("#timer").text("");
+      clearInterval(timeInterval);
+      $("#question-1").hide();
+      $("#Game-Over").removeClass("d-none");
     }
   }
 
   //shows first question
-  $("#questions").removeClass("d-none");
+  $("#question-1").removeClass("d-none");
+
+  $(".btn").click(function nextQuestion() {});
+});
+
+// Answer Function
+$(".correct").click(function correctAnswer() {
+  console.log("correct");
+  localStorage.setItem("highScore", +1);
+});
+
+$(".wrong").click(function wrongAnswer() {
+  console.log("wrong");
+  time = time - 5;
+});
+
+$("#highscore").click(function () {
+  $("#High-Score").removeClass("d-none");
 });
